80ca3828171eb56bb7b69d7960121e630d87e241
iamkimseunghyun <iamkimseunghyun@gmail.com>
2023-02-13 21:48:11 +0900

    스프링부트 3.02 업그레이드

--- src/main/java/io/laaf/project/pharmacy/cache/PharmacyRedisTemplateService.java
+++ src/main/java/io/laaf/project/pharmacy/cache/PharmacyRedisTemplateService.java
@@ -10,7 +10,7 @@ import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
--- src/main/java/io/laaf/project/pharmacy/entity/Pharmacy.java
+++ src/main/java/io/laaf/project/pharmacy/entity/Pharmacy.java
@@ -4,10 +4,10 @@ package io.laaf.project.pharmacy.entity;
 import io.laaf.project.BaseTimeEntity;
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Setter
 @Getter
